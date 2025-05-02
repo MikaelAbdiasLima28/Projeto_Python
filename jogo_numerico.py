3042face06a4a74dc8c26404a5799f6504281bc0
MikaelAbdias <168473785+MikaelAbdiasLima28@users.noreply.github.com>
2025-05-02 08:59:39 -0300

    Update 2 jogo_numerico.py

--- jogo_numerico.py
+++ jogo_numerico.py
@@ -4,7 +4,7 @@ def escolher_dificuldade():
     print("\nEscolha o nível de dificuldade:")
     print("1 - Fácil (1 a 10)")
     print("2 - Médio (1 a 50)")
-    print("3 - Difícil (1 a 1000)")
+    print("3 - Difícil (1 a 100)")
 
     while True:
         escolha = input("Digite o número da dificuldade: ")
